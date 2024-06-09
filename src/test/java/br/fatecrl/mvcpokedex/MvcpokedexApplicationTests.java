27dea36e509e4f4658a4d822877bb54da3cbec3b
IgorCedro <igor9cedro@gmail.com>
2024-06-09 16:48:37 -0300

    Colocando o Framework Spring Boot e adicionando dois pacotes (models e controllers)

---
+++ src/test/java/br/fatecrl/mvcpokedex/MvcpokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fatecrl.mvcpokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcpokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
